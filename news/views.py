9a2b51f935e153248743685585b194f4a94dbb9f
MaryOmoro <maryomoro2017@gmail.com>
2022-12-05 09:20:39 +0300

    add changes in views.py

--- news/views.py
+++ news/views.py
@@ -3,3 +3,15 @@ from django.http import HttpRespose
 # Create your views here.
 def welcome(request):
     return HttpRespose('Welcome to Moringa Tribune')
+
+    import datetime as dt
+    def news-of-the-day(request):
+        date = dt.date.today()
+html = f'''
+<html>
+<body>
+<h1> {date.day}-{date.month}-{date.year}</h1>
+</body>
+</html>
+'''
+return HttpRespose(html)
