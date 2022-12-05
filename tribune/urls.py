29f9881b0f73302aae5b4a01a5711aa5ed3acbcf
MaryOmoro <maryomoro2017@gmail.com>
2022-12-05 08:53:31 +0300

    add changes to urls.py

--- tribune/urls.py
+++ tribune/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url,include
 from django.contrib import admin
-urlpatterns=[
+urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^news/', include('news.urls'))
 ]
