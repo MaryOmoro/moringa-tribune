ceb498ba78852fb409d033aa455d1c5faec03fd5
MaryOmoro <maryomoro2017@gmail.com>
2022-12-05 11:17:04 +0300

    add change to urls.py

--- news/urls.py
+++ news/urls.py
@@ -7,3 +7,6 @@ urlpatterns = [
 urlpatterns = [
     url('^today/$',views.news_of_the_day,name = 'newsToday')
 ]
+urlpatterns = [
+    url(r'^archives/(\d{4}-\d{2}-\d{2})$', views.past_days_news, name = 'pastNews')
+]
