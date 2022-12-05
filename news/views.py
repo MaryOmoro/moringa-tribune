2d408d6e5d43790ceecdd181ee1d5abe64ee03cd
MaryOmoro <maryomoro2017@gmail.com>
2022-12-05 09:46:25 +0300

    add changes in views.py

--- news/views.py
+++ news/views.py
@@ -15,3 +15,22 @@ html = f'''
 </html>
 '''
 return HttpRespose(html)
+def convert_dates(dates):
+day_number = dt.date.weekday(dates)
+days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', "Saturday"]
+day = days[day_number]
+return day
+
+
+def news_of_day(request):
+    date = dt.date.today()
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
